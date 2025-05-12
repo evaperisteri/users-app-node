32b17e76cac8cb53ea254b62e597fa430533f013
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-05-12 19:39:33 +0300

    app.js cors origin

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ const user = require('./routes/user.routes');
 const userProduct = require('./routes/user.products.routes');
 const auth = require('./routes/auth.routes');
 
-// app.use(cors({
-//   origin: ['http://localhost:3000']
-// }))
+app.use(cors({
+  //origin:'*'
+  origin: ['http://localhost:4200']
+}))
 
 app.use('/api/auth', auth);
 app.use('/api/users', user);
